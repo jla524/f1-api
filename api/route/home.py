7a83347470cea18447318cea40a72954bf5471de
Jacky Lee <jla524@sfu.ca>
2021-10-26 23:14:45 -0700

    Remove debug message

--- api/route/home.py
+++ api/route/home.py
@@ -5,5 +5,4 @@ home_api = Blueprint('api', __name__)
 
 @home_api.route('/', methods=['GET'])
 def home():
-    print("Hello")
     return "<h1>F1 API</h1> <p>This site is an API for Formula 1 data.</p>"
