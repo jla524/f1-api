026f59dfc2a4aa56eef54a27375a4ec42d10af46
Jacky Lee <jla524@sfu.ca>
2022-03-03 16:14:39 -0800

    Add script to run through pyproject

---
+++ scripts/project_helper.py
@@ -0,0 +1,42 @@
+"""
+Define functions as project scripts that can be run via pyproject.toml
+"""
+import sys
+import subprocess as sp
+
+from api import Config
+
+
+def run_analyzer() -> None:
+    """
+    Run the mypy static type checker analyzer
+    """
+    path = Config.base_dir() / Config.package()
+    try:
+        sp.run(f'mypy {path}', check=True, shell=True)
+    except sp.CalledProcessError as error:
+        print(error)
+        sys.exit(1)
+
+
+def run_linter() -> None:
+    """
+    Run pylint static code analysis
+    """
+    path = Config.base_dir() / Config.package()
+    try:
+        sp.run(f'pylint {path}', check=True, shell=True)
+    except sp.CalledProcessError as error:
+        print(error)
+        sys.exit(1)
+
+
+def run_tests() -> None:
+    """
+    Run unit tests with pytest
+    """
+    try:
+        sp.run('pytest --capture=tee-sys', check=True, shell=True)
+    except sp.CalledProcessError as error:
+        print(error)
+        sys.exit(1)
